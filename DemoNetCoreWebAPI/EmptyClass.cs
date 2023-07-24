a8ce9e33f30572d5b32efe9b03984f4b6595bd6d
gencode <www@gencode.com>
2023-07-23 20:14:01 -0400

    Test

---
+++ DemoNetCoreWebAPI/EmptyClass.cs
@@ -0,0 +1,59 @@
+﻿/// look at this class - no need to compile or run it, just read it - code part all works correctly 
+/// Dont worry about anything calling or using it, just assume this is the only code that exists - except one line to call it from some service main method, calls every 20 min
+/// it runs on a prod server, whats wrong with it?
+
+using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+public class ApiCaller
+{
+    public async Task<string> CallApi(string apiUrl)
+    {
+        try
+        {
+            using (HttpClient httpClient = new HttpClient())
+            {
+                // Some required get call - required per BRD - yes httpClient could be resused in the every 20 min but ignore that
+                HttpResponseMessage response = await httpClient.GetAsync(apiUrl);
+                response.EnsureSuccessStatusCode();
+
+                // We are done with httpClient, so dispose it before proceeding to ReadFileForFun
+            }
+
+            // Read the fun file - required per BRD
+            ReadFileForFun();
+
+            // We are done - that's all - per BRD
+            return "Success"; // Return "Success" if everything completed without errors
+        }
+        
+        catch (HttpRequestException ex) // throw exception on any httpclient related error
+        {
+            // Log the error and return "Error" if an HTTP error occurs during the API call
+            LogError(ex);
+            return "Error";
+        }
+    }
+
+
+    // Read File
+    private void ReadFileForFun()
+    {
+        // read from this location - required per brd
+        string filePath = @"\\nas-drive\wb-team-rocks\example.txt";
+        // consume - required per brd
+        string fileContent = File.ReadAllText(filePath);
+
+        // return and ignore the content - per brd
+    }
+
+    // Log Error 
+    private void LogError(Exception ex)
+    {
+        //  Log Example - nuget call to log passed in exception - assume this is our common logger and its perfect
+        PerfectNonBlockingLoggingNuGet.Log.Error(ex, "An error occurred while calling the API.");
+    }
+
+}
